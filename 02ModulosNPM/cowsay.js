b242a087fa561835435331e668ce2e35e48723df
Pablo Guisasola <pabloguibe01@gmail.com>
2023-01-09 16:32:50 +0100

    pruebas modulos

---
+++ 02ModulosNPM/cowsay.js
@@ -0,0 +1,9 @@
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+   text : "¡Hola a la chavalería!",
+   e : "oO",
+   T : "U "
+}));
+
+// or cowsay.think()
---
+++ 03ModulosNPM/cowsay.js
@@ -0,0 +1,14 @@
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+   text : "¡Hola a la chavalería!",
+   e : "oO",
+   T : "U "
+}));
+
+console.log(cowsay.say({
+   text : "¡Hola a la chavalería!",
+   e : "oO",
+   T : "U "
+}));
+// or cowsay.think()
