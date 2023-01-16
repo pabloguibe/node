da21b4ec9fdb445a182f5d62b2a7f2e9551b1517
Pablo Guisasola <pabloguibe01@gmail.com>
2023-01-16 16:40:20 +0100

    DBPokemon

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3005
 
-//Motor de plantillas
-app.set("views", __dirname + '/views');
-app.set("view engine", 'ejs');
+require('dotenv').config()
+const port = process.env.PORT || 3005
+
+//Conexión a base de datos
+const mongoose = require('mongoose');
+//Variables que tendremos siempre:
+//Lo correcto será declararlas EN VARIABLES DE ENTORNO
+//para que nadie vea directamente nuestras credenciales
+const uri = `mongodb+srv://${process.env.DB_USSER}:${process.env.DB_PASSWORD}@cluster0.eg1v6lz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`; //URL de conexión, que completaremos luego
+mongoose.connect(uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
 
 
 //Middleware
 app.use(express.static(__dirname + '/public'));
+
+//Motor de plantillas
+app.set("views", __dirname + '/views');
+app.set("view engine", 'ejs');
+app.use('/', require('./router/rutas'))
+app.use('/pokemon', require('./router/pokemon'))
 
 app.get('/', (req, res) => {
     res.render("index", {titulo: "mi titulo dinamico"})
---
+++ 03Express/router/pokemon.js
@@ -0,0 +1,19 @@
+const express = require('express') 
+const router = express.Router();
+const Pokemon = require('../models/pokemon');
+
+router.get('/', async (req, res) => {
+    try {
+        //Le pondremos arrayPokemonDB para diferenciar
+        //los datos que vienen de la base de datos
+        //con respecto al arrayPokemon que tenemos EN LA VISTA
+        const arrayPokemonDB = await Pokemon.find();
+        console.log(arrayPokemonDB);
+        res.render("pokemon", { 
+            arrayPokemon: arrayPokemonDB
+        })
+    } catch (error) {
+        console.error(error)
+    }
+})
+module.exports = router;
