d520da0945b39fc57ff834ebf7c7ff8da7528951
Pablo Guisasola <pabloguibe01@gmail.com>
2023-01-23 18:16:30 +0100

    pokemon2

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser  = require('body-parser');
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 
 require('dotenv').config()
 const port = process.env.PORT || 3005
@@ -23,16 +28,9 @@ app.use(express.static(__dirname + '/public'));
 //Motor de plantillas
 app.set("views", __dirname + '/views');
 app.set("view engine", 'ejs');
+
 app.use('/', require('./router/rutas'))
 app.use('/pokemon', require('./router/pokemon'))
-
-app.get('/', (req, res) => {
-    res.render("index", {titulo: "mi titulo dinamico"})
-  })
-
-app.get('/contacto', (req, res) => {
-    res.render("contacto", {tituloContacto: "Estamos en contacto de manera dinamica"})
-})
 
 app.use((req,res) => {
     res.status(404).render("404", {titulo: "ERROR 404", descripcion: "Page Not Found"})
